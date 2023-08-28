18d432f5538ccc5b54ba3bfd2de72097b8f8d8b3
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-28 18:17:17 +0530

    introduce a new variable to hold on to the value of switch values of two variables.

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ fn generate_fibonacci_sequence_up_to_n(n: u32) -> u32{
     let mut a = 0;
     let mut b = 1;
     for i in 0..n {
+        let temp = a;
         a = b;
-        b += a;
+        b += temp;
     }
     b
 }
