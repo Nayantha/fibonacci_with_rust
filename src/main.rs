91bff1ca8f9ddcbcde83ba959f8f815cf0d6935e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-28 18:20:00 +0530

    change for loop to while.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
-fn generate_fibonacci_sequence_up_to_n(n: u32) -> u32{
+fn generate_fibonacci_sequence_up_to_n(mut n: u32) -> u32{
     let mut a = 0;
     let mut b = 1;
-    for i in 0..n {
+    while n > 0 {
         let temp = a;
         a = b;
         b += temp;
+         n -= 1;
     }
     b
 }
