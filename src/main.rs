34756c818f7adae8fa3d7f91b6259d55cf9f61fd
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-28 18:35:50 +0530

    add logic to use the recursion fibonacci function.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ fn main() {
     for i in 0..10 {
         println!("{}th Fibonacci number is: {}", i, generate_fibonacci_sequence_up_to_n(i))
     }
+    for i in 0..10 {
+        println!("{}th Fibonacci number is: {}", i, generate_fibonacci_sequence_up_to_n_with_recursion(i))
+    }
 }
 fn generate_fibonacci_sequence_up_to_n(mut n: u32) -> u32{
     let mut a = 0;
