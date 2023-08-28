646364c524c2a559248576e13db1721644c2323e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-28 19:06:41 +0530

    remove function all calls.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-use std::time::Instant;
 fn main() {
     println!("Hello, welcome to Fibonacci Sequence Generator!");
-    let mut start_time = Instant::now();
-    for i in 0..10 {
-        println!("{}th Fibonacci number is: {}", i, generate_fibonacci_sequence_up_to_n(i))
-    }
-    let mut end_time = Instant::now();
-    println!("{}",start_time.duration_since(end_time).as_nanos());
-    start_time = Instant::now();
-    for i in 0..10 {
-        println!("{}th Fibonacci number is: {}", i, generate_fibonacci_sequence_up_to_n_with_recursion(i))
-    }
-    end_time = Instant::now();
-    println!("{}",start_time.duration_since(end_time).as_nanos());
 }
 fn generate_fibonacci_sequence_up_to_n(mut n: u32) -> u32{
     let mut a = 0;
