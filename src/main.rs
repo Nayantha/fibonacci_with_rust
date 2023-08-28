3f11228d6b7cb55b1dc4a4c50ea65539c2092005
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-28 18:14:43 +0530

    add fibonacci sequence generator function.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
 }
+fn generate_fibonacci_sequence_up_to_n(n: u32) -> u32{
+    let mut a = 0;
+    let mut b = 1;
+    for i in 0..n {
+        a = b;
+        b += a;
+    }
+    b
+}
